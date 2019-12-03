89703ac26bca2f6c999a697fe833fc76685de549
NuiS4ncE <nuis4nce@hotmail.com>
2019-12-03 20:04:04 +0200

    Done 2nd part

--- FirstDayRocket.py
+++ FirstDayRocket.py
@@ -1,12 +1,17 @@
 import numpy
 
-
 def main():
     summa = 0
-
+    gaga = 0
     f = open("List.txt", "r")
     for x in f:
         summa += int(int(x)/3)-2
-    print(summa)
+        numba = int(int(x)/3)-2
+        while numba >= 0:
+            gaga += numba
+            numba = int(int(numba)/3)-2
+
+    print("First answer: {}" .format(summa))
+    print("Second answer: {}" .format(gaga))
 
 main()
