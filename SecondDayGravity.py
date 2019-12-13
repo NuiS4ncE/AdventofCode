9d34a5f8e96f2c9daa27e522bf6481eebcbbab39
NuiS4ncE <nuis4nce@hotmail.com>
2019-12-13 18:43:37 +0200

    2nd day part two done

--- SecondDayGravity.py
+++ SecondDayGravity.py
@@ -1,23 +1,25 @@
+from random import seed
+from random import random
+
 f = open("List2.txt", "r")
 y = f.read().rstrip().split(",")
 d = list(map(int, y))
 
 
-def main():
-    write(1, 12)
-    write(2, 2)
+def main(x):
     i = 0
     while i < len(d):
-        if (d[i] == 1):
-            opcode1(i)
-        if (d[i] == 2):
-            opcode2(i)
-        if (d[i] == 99):
+        if (x[i] == 1):
+            opcode1(x, i)
+        if (x[i] == 2):
+            opcode2(x, i)
+        if (x[i] == 99):
             break
         i = i + 4
-    print(d[0])
+    #print(d[0])
 
-def opcode1(i):
+
+def opcode1(d, i):
     a = d[i + 3]
     b = d[i + 1]
     c = d[i + 2]
@@ -25,7 +27,7 @@ def opcode1(i):
     return
 
 
-def opcode2(i):
+def opcode2(d, i):
     a = d[i + 3]
     b = d[i + 1]
     c = d[i + 2]
@@ -36,6 +38,17 @@ def opcode2(i):
 def write(x, y):
     d[x] = y
 
+def findComb(x):
+    for y in range(99):
+        for z in range(99):
+            e = [int(a) for a in d]
+            e[1] = y
+            e[2] = z
+            try:
+                main(e)
+            except IndexError:
+                pass
+            if (e[0] == x):
+                print(f"{e[0]} with numbers {y} and {z}. Thus 100 * ")
 
-
-main()
+findComb(19690720)
